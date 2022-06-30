84f471fb14d35077200183234c0c6b295ad0e5f9
Unknown developer <83854084+unknownhackerfromindia@users.noreply.github.com>
2022-06-30 20:24:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
+import requests
 import json
 import subprocess
 from pyrogram.types.messages_and_media import message
+import helper
 from pyromod import listen
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message
 import pyrogram
+import logging
 from pyrogram import Client, filters
+from subprocess import getstatusoutput
+import re
 from pyrogram.types import User, Message
 import os
+
 import requests
 bot = Client(
-    "Careerwill",
+    "CW",
     bot_token=os.environ.get("BOT_TOKEN"),
     api_id=int(os.environ.get("API_ID")),
     api_hash=os.environ.get("API_HASH")
 )
+logger = logging.getLogger()
+thumb = os.environ.get("THUMB")
+if thumb.startswith("http://") or thumb.startswith("https://"):
+    getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
+    thumb = "thumb.jpg"
 @bot.on_message(filters.command(["start"]))
 async def start(bot, update):
-       await update.reply_text("Hi {firstname} i can download PDFs/NOTEs from Careerwill.\n\n")
-
-
-
-
-
-
+       await update.reply_text("Hi i am **physics wallah Downloader**.\n\n"
+                              "**NOW:-** "
+                                       
+                                       "Press **/login** to continue..\n\n")
 bot.run()
+                                     
